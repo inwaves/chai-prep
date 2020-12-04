1d975276c04d912082d02873964bd3cc5bbc96c9
Andrei <aalexandru@ezesoft.com>
2020-12-04 18:21:33 +0200

    Possibly completed the algorithm

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 chai-prep
 ---
 
-What's in a decision tree?
+### What's in a decision tree?
 - structured data
   [x] examples consisting of features + an output label
   [x] a list of feature names
@@ -11,8 +11,8 @@ What's in a decision tree?
   [x] leaves: contain just a result, an output
 - the learning algorithm
 
-What does the learning algorithm do?
-[] it finds the best attribute according to some measure
+### What does the learning algorithm do?
+[x] it finds the best attribute according to some measure
 [x] it uses the current node to expand values for that attribute
 [x] it stores the possible values as branches leading to other nodes
 - it does this until:
@@ -20,9 +20,11 @@ What does the learning algorithm do?
   [x] all the examples in this node have the same output——it returns the result
   [x] there are no more examples (no examples had this value of the previous attribute)——it returns the result of its parent
 
-What auxiliary functions does it need?
-- something to calculate the best attribute
-- something to calculate the information gain
-[x] something to find examples with a given value
-[x] something to find unique values for an attribute
-[x] something to calculate the frequency of each value of an attribute among a set of examples
+### What auxiliary functions does it need?
+[x] calculate the best attribute
+[x] calculate the information gain
+    [x] calculate entropy
+[x] find examples with a given value
+[x] find unique values for an attribute
+[x] calculate the frequency of each value of an attribute among a set of examples
+[] good representations for all types of data so the tree output is understandable
--- will_wait.py
+++ will_wait.py
@@ -1,78 +1,131 @@
+import math
+
+
 class DataSet:
     def __init__(self, examples: list, attr_names: list, target_index: int) -> None:
         self.examples = examples
         self.attr_names = attr_names
         self.attr = range(len(attr_names))
-        self.target_index  = target_index # which column of the example is the output
-    
-    def examples_with_value(self, examples: list, value: str) -> list:
-        return [example for example in examples if example[self.target] == value]
-    
+        self.target_index = target_index  # which column of the example is the output
+
+    def examples_with_value(self, examples: list, attribute: int, value: str) -> list:
+        return [example for example in examples if example[attribute] == value]
+
     def unique_values(self, examples: list, attr: int) -> set:
         return set(example[attr] for example in examples)
-    
+
     def most_common_value_of_attribute(self, examples: list, attr: int) -> str:
         unique_vals = self.unique_values(examples, attr)
         max_count, max_val = 0, 0
-        
+
         for val in unique_vals:
-            current_count = self.examples_with_value(examples, val)
+            current_count = self.examples_with_value(examples, attr, val)
             if current_count > max_count:
-                max_count = current_count
-                max_val = val
-        
+                max_count, max_val = current_count, val
+
         return max_val
-        
-    
+
+
 class Leaf:
     def __init__(self, result) -> None:
         self.examples = result
-        
+
+
 class DecisionNode:
-    def __init__(self, attr: str, branches: dict=None) -> None:
+    def __init__(self, attr: str, branches: dict = None) -> None:
         self.attr = attr
         self.branches = branches
-        
+
+
 class DecisionTreeLearner:
-    
     def __init__(self, dataset: DataSet) -> None:
         self.dataset = dataset
-        
-    def decision_tree_learner(self, examples: list, 
-                              attributes: list, 
-                              parent_examples: list) -> DecisionNode:
-        
+
+    def decision_tree_learner(
+        self, examples: list, attributes: list, parent_examples: list
+    ) -> DecisionNode:
+
         # check for stoppage first
         # no more attributes
         if len(attributes) == 0:
-            return self.dataset.most_common_value_of_attribute(examples, self.dataset.target_index)
-        
+            return self.dataset.most_common_value_of_attribute(
+                examples, self.dataset.target_index
+            )
+
         # all examples have the same output
         unique_outputs = self.dataset.unique_values(examples, self.dataset.target_index)
         if len(unique_outputs) == 1:
             return unique_outputs[0]
-        
+
         # there are no more examples
         if len(examples) == 0:
-            return self.dataset.most_common_value_of_attribute(parent_examples, self.dataset.target_index)
+            return self.dataset.most_common_value_of_attribute(
+                parent_examples, self.dataset.target_index
+            )
 
         # no stoppage? continue with algorithm
         # find the best attribute
         attr = self.find_best_attribute(examples, attributes)
         tree = DecisionNode(attr)
-        
-        attributes.pop(attr)    # remove the attribute we've just used
-        
+
+        attributes.pop(attr)  # remove the attribute we've just used
+
         # use the current node to expand values for that attribute
         for val in self.dataset.unique_values(examples, attr):
-            subtree = self.decision_tree_learner(self.dataset.examples_with_value(examples, val),
-                                                 attributes,
-                                                 examples)
+            subtree = self.decision_tree_learner(
+                self.dataset.examples_with_value(examples, attr, val),
+                attributes,
+                examples,
+            )
             tree.branches[val] = subtree
-        
+
         return tree
-    
+
     def find_best_attribute(self, examples: list, attributes: list) -> int:
-        pass
-    
+
+        # go through the list of attributes,
+        # calculate its information gain
+        max_gain, best_attr = 0, 0
+        for attr in attributes:
+            current_gain = self.information_gain(examples, attr)
+            if current_gain > max_gain:
+                max_gain, best_attr = current_gain, attr
+
+        return best_attr
+
+    def information_gain(self, examples: list, attr: int) -> float:
+
+        # calculate the current entropy
+        current_entropy = self.entropy(examples)
+        total_examples = len(examples)
+        average_child_entropy = 0.0
+
+        # calculate the average entropy of child nodes
+        for val in self.dataset.unique_values(examples, attr):
+            examples_with_val = self.dataset.examples_with_value(examples, attr, val)
+            average_child_entropy += len(
+                examples_with_val + self.entropy(examples_with_val)
+            )
+
+        average_child_entropy /= total_examples
+
+        # return difference, i.e. information gain
+        return current_entropy - average_child_entropy
+
+    def entropy(self, examples: list) -> float:
+        total_examples = len(examples)
+        unique_outputs = self.dataset.unique_values(examples, self.dataset.target_index)
+        entropy = 0.0
+        for output in unique_outputs:
+            output_probability = (
+                self.dataset.examples_with_value(
+                    examples, self.dataset.target_index, output
+                )
+                / total_examples
+            )
+            entropy -= output_probability * math.log2(output_probability)
+            
+        return entropy
+
+if __name__ == "__main__":
     
